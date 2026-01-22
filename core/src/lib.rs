47f5dd437235b380ad11d17f2f6c9408fddcdd8b
Steve Hill <steve@stevehill.xyz>
2026-01-22 15:28:20 +0000

    Add initial skeleton implementation for the Z80

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,9 +1,9 @@
 //! Core traits for retro computer emulation.
 
 mod bus;
-mod io_bus;
 mod cpu;
+mod io_bus;
 
 pub use bus::Bus;
-pub use io_bus::IoBus;
 pub use cpu::Cpu;
+pub use io_bus::IoBus;
--- cpu-z80/src/lib.rs
+++ cpu-z80/src/lib.rs
@@ -1,14 +1,95 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! Z80 CPU emulator.
+
+use emu_core::{Cpu, IoBus};
+
+/// The Z80 CPU state.
+pub struct Z80 {
+    // Main registers
+    a: u8,
+    f: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    h: u8,
+    l: u8,
+
+    // Shadow registers
+    a_shadow: u8,
+    f_shadow: u8,
+    b_shadow: u8,
+    c_shadow: u8,
+    d_shadow: u8,
+    e_shadow: u8,
+    h_shadow: u8,
+    l_shadow: u8,
+
+    // Index registers
+    ix: u16,
+    iy: u16,
+
+    // Other registers
+    sp: u16,
+    pc: u16,
+    i: u8,
+    r: u8,
+
+    // Interrupt state
+    iff1: bool,
+    iff2: bool,
+    interrupt_mode: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Z80 {
+    pub fn new() -> Self {
+        Self {
+            a: 0,
+            f: 0,
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            h: 0,
+            l: 0,
+            a_shadow: 0,
+            f_shadow: 0,
+            b_shadow: 0,
+            c_shadow: 0,
+            d_shadow: 0,
+            e_shadow: 0,
+            h_shadow: 0,
+            l_shadow: 0,
+            ix: 0,
+            iy: 0,
+            sp: 0xFFFF,
+            pc: 0,
+            i: 0,
+            r: 0,
+            iff1: false,
+            iff2: false,
+            interrupt_mode: 0,
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<B: IoBus> Cpu<B> for Z80 {
+    fn step(&mut self, bus: &mut B) -> u32 {
+        todo!()
+    }
+
+    fn reset(&mut self, bus: &mut B) {
+        todo!()
+    }
+
+    fn interrupt(&mut self, bus: &mut B) {
+        todo!()
+    }
+
+    fn nmi(&mut self, bus: &mut B) {
+        todo!()
+    }
+
+    fn pc(&self) -> u16 {
+        self.pc
     }
 }
